406a57790b075f7a6e2ef8d6382c767f58d43b00
Sav Younts <savyounts@gmail.com>
2018-09-05 18:13:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
-
+require 'securerandom'
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item]= params[:item]
+
+    erb :checkout
+  end
 end
