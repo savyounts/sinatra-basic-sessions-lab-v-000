fbe112ecd8fa6e176222a29a12c27ca92c1e5a9c
Sav Younts <savyounts@gmail.com>
2018-09-05 17:58:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
-
+require 'securerandom'
 class App < Sinatra::Base
+
+  configure do 
+    enable :sessions
+    set :session_secret, puts SecureRandom.hex(64)
 end
