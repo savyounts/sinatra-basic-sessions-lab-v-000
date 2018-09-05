c7a1003aedd4753f304d413bc3bfcefe7e357d9d
Sav Younts <savyounts@gmail.com>
2018-09-05 18:11:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, puts SecureRandom.hex(64)
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end
 
   get '/' do
